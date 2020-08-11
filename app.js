db0dec724bf15bf6e42844fd61a7215f5720e899
Saad <mohammadsaadjavediqbal1999@gmail.com>
2020-08-11 19:24:31 +0500

    Quiz App

---
+++ app.js
@@ -0,0 +1,121 @@
+function check(){
+    var score = 0;
+    var Q1option_1 = document.getElementById("a11");
+    var Q1option_2 = document.getElementById("a12");
+    var Q1answer = document.getElementById("a13");
+    var Q1option_4 = document.getElementById("a14");
+    if(Q1answer.checked == true){
+score++
+
+    }
+
+
+
+
+var Q2answer = document.getElementById("a21");
+var Q2option_2 = document.getElementById("a22");
+var Q2option_3 = document.getElementById("a23");
+var Q2option_4 = document.getElementById("a24");
+if(Q2answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q3answer = document.getElementById("a31");
+var Q3option_2 = document.getElementById("a32");
+var Q3option_3 = document.getElementById("a33");
+var Q3option_4 = document.getElementById("a34");
+if(Q3answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q4option_1 = document.getElementById("a41");
+var Q4option_2 = document.getElementById("a42");
+var Q4option_3 = document.getElementById("a43");
+var Q4answer = document.getElementById("a44");
+if(Q4answer.checked == true){
+score++
+
+}
+
+
+
+var Q5option_1= document.getElementById("a51");
+var Q5option_2 = document.getElementById("a52");
+var Q5answer = document.getElementById("a53");
+var Q5option_4 = document.getElementById("a54");
+if(Q5answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q6answer = document.getElementById("a61");
+var Q6option_2 = document.getElementById("a62");
+var Q6option_3 = document.getElementById("a63");
+var Q6option_4 = document.getElementById("a64");
+if(Q6answer.checked == true){
+score++
+
+}
+
+
+
+var Q7option_1= document.getElementById("a71");
+var Q7answer  = document.getElementById("a72");
+var Q7option_3 = document.getElementById("a73");
+var Q7option_4 = document.getElementById("a74");
+if(Q7answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q8option_1= document.getElementById("a81");
+var Q8answer  = document.getElementById("a82");
+var Q8option_3 = document.getElementById("a83");
+var Q8option_4 = document.getElementById("a84");
+if(Q8answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q9option_1 = document.getElementById("a91");
+var Q9option_2 = document.getElementById("a92");
+var Q9answer = document.getElementById("a93");
+var Q9option_4 = document.getElementById("a94");
+if(Q9answer.checked == true){
+score++
+
+}
+
+
+
+
+var Q10answer = document.getElementById("a101");
+var Q10option_2 = document.getElementById("a102");
+var Q10option_3 = document.getElementById("a103");
+var Q10option_4 = document.getElementById("a104");
+if(Q10answer.checked == true){
+score++
+
+}
+
+
+
+alert("Your Score is " + score++);
+}
